4a5f09ad60a524b03b9dcebac77189ce0e621eae
msfs1992 <msfs1992@gmail.com>
2018-07-23 04:10:27 -0300

    Change

--- src/android/BSRV.java
+++ src/android/BSRV.java
@@ -1,37 +1,39 @@
 package you.got.it.plugin;
 
-    import org.apache.cordova.CordovaPlugin;
-    import org.apache.cordova.CallbackContext;
-    import android.content.Intent;
-    import org.json.JSONArray;
-    import org.json.JSONException;
-    import org.json.JSONObject;
+import android.app.Activity;
+import android.os.IBinder;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import android.content.Intent;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    /**
-     * This class echoes a string called from JavaScript.
-     */
-    public class BSRV extends CordovaPlugin {
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class BSRV extends CordovaPlugin {
 
-        @Override
-        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-            if (action.equals("echo")) {
-                String message = args.getString(0);
-                this.echo(message, callbackContext);
-                return true;
-            }
-            return false;
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("echo")) {
+            String message = args.getString(0);
+            this.echo(message, callbackContext);
+            return true;
         }
+        return false;
+    }
 
-        private void echo(String message, CallbackContext callbackContext) {
-            Activity context = cordova.getActivity();
-            if (message != null && message.length() > 0) {
+    private void echo(String message, CallbackContext callbackContext) {
+        Activity context = cordova.getActivity();
+        if (message != null && message.length() > 0) {
 
-                Intent myServiceIntent = new Intent(context, performSync.class);
-                //fireEvent(Event.ACTIVATE, null);
-                context.startService(myServiceIntent);
-                callbackContext.success(message);
-            } else {
-                callbackContext.error("Expected one non-empty string argument.");
-            }
+            Intent myServiceIntent = new Intent(context, performSync.class);
+            //fireEvent(Event.ACTIVATE, null);
+            context.startService(myServiceIntent);
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
         }
     }
+}
