1be478c6d3226a49e945532580d2d4668365c4e9
msfs1992 <msfs1992@gmail.com>
2018-07-22 22:24:58 -0300

    Push

--- www/bsrv.js
+++ www/bsrv.js
@@ -1,6 +1,5 @@
 var BSRV = {
     initserv: function(successCallback, failureCallback) {
-    	console.log("hola");
         cordova.exec(successCallback, failureCallback, 'BSRV',
             'echo', ["Marcello Fabbiani"]);
     }
